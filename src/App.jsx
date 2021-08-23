1a469daca055946892369bb19a0e7c58162a60ed
ameybhojane <88000728+ameybhojane@users.noreply.github.com>
2021-08-23 15:16:11 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ReactDOM } from 'react';
+import Content from './components/Content';
+import Header from './components/Header';
+import "./components/App.scss"
+
+const App = () => {
+    return (
+<>
+<Header/>
+<Content/>
+</>
+    )
+}
+
+export default App;
